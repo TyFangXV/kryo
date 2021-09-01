37826b352a8e47b4e3416176bef797c2e0ecdcde
Tyfang.dev <68788849+TyFangXV@users.noreply.github.com>
2021-09-01 12:33:16 +0400

    Update joke.js

--- src/command/fun/joke.js
+++ src/command/fun/joke.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
   name: path.basename(__filename).split(".")[0],
-  status : "dep",
+  status : "dev",
   async execute(message) {
     let data = await fx.dad_joke();
     message.channel.send(data);
